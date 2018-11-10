87169bfed0f3078a7758b02900c44a2c7892c5e6
PTSoup <phaimani.souphaphone@gmail.com>
2018-11-10 14:33:51 -0600

    Complete implementing GroceryListItem component

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // TODO
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+  </ul>
+)
+
+
+
 var GroceryList = () => (
   <div>
-    <ul>
-      <ChewingGum />
-      <Socks />
-    </ul>
+   <h2>My Grocery List</h2>
+    <GroceryListItem groceryItems={['tacos', 'socks']}/>
   </div>
 );
 
-var ChewingGum = () => (
-  <li>Chewing Gum</li>
-);
+// var ChewingGum = () => (
+//   <li>Chewing Gum</li>
+// );
 
-var Socks = () => (
-  <li>Socks</li>
-);
+// var Socks = () => (
+//   <li>Socks</li>
+// );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
