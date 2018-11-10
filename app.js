f00ea47c0abe508bdfb8963481680d6ed60bc3ce
PTSoup <phaimani.souphaphone@gmail.com>
2018-11-10 14:10:43 -0600

    Complete creating the grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // TODO
+var GroceryList = () => (
+  <div>
+    <ul>
+      <li>Chewing Gum</li>
+      <li>Socks</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
