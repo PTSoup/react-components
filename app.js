73e94ea711434fff58008deabe94433a937a636b
PTSoup <phaimani.souphaphone@gmail.com>
2018-11-11 00:57:06 -0600

    Complete Bare Minimum Requirements

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// TODO
+// // TODO
 var GroceryList = (props) => (
   <ul>
     {props.groceryItems.map(item =>
@@ -11,14 +11,27 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
     );
   }
-
 }
 
 ReactDOM.render(<GroceryList groceryItems={['Chewing Gum','Socks']} />, document.getElementById("app"));
